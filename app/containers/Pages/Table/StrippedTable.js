8d67610665ae50e7b515243af8bb2f4e7d6c7497
shangharsha <idkinachaiyo@gmail.com>
2021-10-21 13:12:07 +0545

    TABLE UPDATED

--- app/containers/Pages/Table/StrippedTable.js
+++ app/containers/Pages/Table/StrippedTable.js
@@ -9,68 +9,79 @@ import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
+import Button from '@material-ui/core/Button';
 import styles from 'dan-components/Tables/tableStyle-jss';
+import dbs from '../../../../db.json';
 
-let id = 0;
-function createData(name, calories, fat, carbs, protein) {
-  id += 1;
-  return {
-    id,
-    name,
-    calories,
-    fat,
-    carbs,
-    protein
-  };
-}
-
-const data = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
-];
+// let id = 0;
+// function createData(name, calories, fat, carbs, protein) {
+// 	id += 1;
+// 	return {
+// 		id,
+// 		name,
+// 		calories,
+// 		fat,
+// 		carbs,
+// 		protein,
+// 	};
+// }
 
 function StrippedTable(props) {
-  const { classes } = props;
-  return (
-    <Fragment>
-      <Toolbar className={classes.toolbar}>
-        <div className={classes.title}>
-          <Typography className={classes.title} variant="h6">Nutrition</Typography>
-        </div>
-      </Toolbar>
-      <div className={classes.rootTable}>
-        <Table className={classNames(classes.table, classes.stripped)}>
-          <TableHead>
-            <TableRow>
-              <TableCell padding="default">Dessert (100g serving)</TableCell>
-              <TableCell align="right">Calories</TableCell>
-              <TableCell align="right">Fat (g)</TableCell>
-              <TableCell align="right">Carbs (g)</TableCell>
-              <TableCell align="right">Protein (g)</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {data.map(n => ([
-              <TableRow key={n.id}>
-                <TableCell padding="default">{n.name}</TableCell>
-                <TableCell align="right">{n.calories}</TableCell>
-                <TableCell align="right">{n.fat}</TableCell>
-                <TableCell align="right">{n.carbs}</TableCell>
-                <TableCell align="right">{n.protein}</TableCell>
-              </TableRow>
-            ]))}
-          </TableBody>
-        </Table>
-      </div>
-    </Fragment>
-  );
+	const { classes } = props;
+	function handleRemove(id) {
+		const fdata = dbs.filter((item) => item.id !== id);
+		console.log(fdata);
+	}
+
+	return (
+		<Fragment>
+			<Toolbar className={classes.toolbar}>
+				<div className={classes.title}>
+					<Typography className={classes.title} variant='h6'>
+						Nutrition
+					</Typography>
+				</div>
+			</Toolbar>
+			<div className={classes.rootTable}>
+				<Table className={classNames(classes.table, classes.stripped)}>
+					<TableHead>
+						<TableRow>
+							<TableCell padding='default'>Dessert (100g serving)</TableCell>
+							<TableCell align='right'>Calories</TableCell>
+							<TableCell align='right'>Fat (g)</TableCell>
+							<TableCell align='right'>Carbs (g)</TableCell>
+							<TableCell align='right'>Protein (g)</TableCell>
+							<TableCell align='center'>Action </TableCell>
+						</TableRow>
+					</TableHead>
+					<TableBody>
+						{dbs.map((n) => [
+							<TableRow key={n.id}>
+								<TableCell padding='default'>{n.name}</TableCell>
+								<TableCell align='right'>{n.calories}</TableCell>
+								<TableCell align='right'>{n.fat}</TableCell>
+								<TableCell align='right'>{n.carbs}</TableCell>
+								<TableCell align='right'>{n.protein}</TableCell>
+								<TableCell align='center'>
+									<Button color='secondary'>Edit</Button>|
+									<Button
+										color='secondary'
+										value={n.id}
+										onClick={() => handleRemove(n.id)}
+									>
+										Delete
+									</Button>
+								</TableCell>
+							</TableRow>,
+						])}
+					</TableBody>
+				</Table>
+			</div>
+		</Fragment>
+	);
 }
 
 StrippedTable.propTypes = {
-  classes: PropTypes.object.isRequired,
+	classes: PropTypes.object.isRequired,
 };
-
 export default withStyles(styles)(StrippedTable);
